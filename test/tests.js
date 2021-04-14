5c2d9784292288454d9fd0557c96761ab2b6ec5f
sl1ked <viktor.butenko.00@gmail.com>
2021-04-14 23:22:00 +0300

    add: comment for code

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,17 @@
-var chai = require("chai");
-var testCase = require("mocha").describe;
-var chaiHttp = require("chai-http");
-var should = chai.should();
+let chai = require("chai");
+let testCase = require("mocha").describe;
+let chaiHttp = require("chai-http");
+let should = chai.should();
 const fetch = require("node-fetch");
 
 chai.use(chaiHttp);
 
 // test for GET	/posts
+/*
+  input: get/posts
+  output: 1. Status code=200;
+          2. Array is lenght=100 
+*/
 testCase("/GET posts", function () {
   it("it should GET all the posts", (done) => {
     chai
@@ -19,6 +24,14 @@ testCase("/GET posts", function () {
       });
   });
 
+/*
+input: correct id  and title
+output: 1. Status code=200;
+        2. Array of objects
+          every object have:
+            a) property id and body 
+            b) value id=input id and title=input title
+*/
   it("it should GET post  on CORRECT id and CORRECT title", (done) => {
     const title = "laboriosam dolor voluptates";
     const id = 10;
@@ -37,17 +50,21 @@ testCase("/GET posts", function () {
         setTimeout(
           () =>
             console.log(
-              "\x1b[33m\x1b[40m%s\x1b[0m ",
-              `ID:${id} title="${title}"`,
+              "\x1b[33m\x1b[40m%s\x1b[0m",
+              `ID=${id};title="${title}"\n`,
               res.body
             ),
           0
         );
-        // console.log(res.body)
         done();
       });
   });
 
+/*
+input: empty id  and correct title
+output: 1. Status code=200;
+        2. Empty array
+*/
   it("it should GET random post on CORRECT title and EMPTY id", (done) => {
     const title = "laboriosam dolor voluptates";
     const id = "";
@@ -60,6 +77,12 @@ testCase("/GET posts", function () {
         done();
       });
   });
+
+/*
+input:  correct id  and empty title
+output: 1. Status code=200;
+        2. Empty array
+*/
   it("it should GET random post on  EMPTY title and CORRECT id", (done) => {
     const title = "";
     const id = "10";
@@ -72,6 +95,12 @@ testCase("/GET posts", function () {
         done();
       });
   });
+
+/*
+input: empty id  and empty title
+output: 1. Status code=200;
+        2. Empty array
+*/
   it("it should GET random post on EMPTY title and EMPTY id", (done) => {
     const title = "";
     const id = "";
@@ -86,48 +115,3 @@ testCase("/GET posts", function () {
   });
 });
 
-// it("it should GET random post on ID", (done) => {
-//   const maxValueID = 10;
-//   // I'm not sure! May be, random userId is bad
-//   const randomIntNumber = parseInt(Math.random() * maxValueID + 1);
-//   chai
-//     .request("https://jsonplaceholder.typicode.com")
-//     .get(`/posts?userId=` +randomIntNumber)
-//     .end((err, res) => {
-//       res.should.have.status(200);
-//       res.body.should.be.not.empty;
-//       res.body.forEach(el=> {
-//         el.should.include({"userId":randomIntNumber});
-//       });
-//       console.log("search ID=",randomIntNumber)
-//       const numberPrintPost=2;
-//       const startSlicePosition=0;
-//       console.log(res.body.slice(startSlicePosition,numberPrintPost))
-//       done();
-//     });
-// });
-// it("it should GET random post on Not correct ID", (done) => {
-//   const NoCorectID = -1;
-//   chai
-//     .request("https://jsonplaceholder.typicode.com")
-//     .get(`/posts?userId=` +NoCorectID)
-//     .end((err, res) => {
-//       res.should.have.status(200);
-//       done();
-//     });
-// });
-// it("it should GET random post  on title", (done) => {
-//   const title="laboriosam dolor voluptates"
-//   chai
-//     .request("https://jsonplaceholder.typicode.com")
-//     .get(`/posts?title=` +title)
-//     .end((err, res) => {
-//       res.should.have.status(200);
-//       res.body.should.be.not.empty;
-//       console.log(res.body)
-//       done();
-//     });
-// });
-/*
-  
-  */
